67593284ada172659155564ceb10453bbe5a849b
Ramesh Polishetti <ramesh.polishetti@macys.com>
2016-01-19 08:39:35 +0530

    updated feedback.html

--- public/js/templates/technologies.js
+++ public/js/templates/technologies.js
@@ -1,7 +1,7 @@
 define(['handlebars'], function(Handlebars) {
 
 return Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "\r\n            <div class=\"row\" style=\"text-align: center\">\r\n                <h2>Requests and Suggestions</h2>\r\n            </div>\r\n        ";
+    return "\n            <div class=\"row\" style=\"text-align: center\">\n                <h2>Requests and Suggestions</h2>\n            </div>\n        ";
 },"useData":true})
 
 });
--- public/js/views/session.js
+++ public/js/views/session.js
@@ -1,224 +1,234 @@
 define(['jquery', 'backbone', 'templates/session',
-    'templates/sessionDetailsSub', 'appUser', 'collections/participants',
-    'views/participantRow'
-  ],
-  function($, Backbone, sessionTemplate, sessionDetailsSubTemplate, appUser, ParticipantsCollection, ParticipantRowView) {
+        'templates/sessionDetailsSub', 'appUser', 'collections/participants',
+        'views/participantRow', 'templates/overlays/feedback'
+    ],
+    function($, Backbone, sessionTemplate, sessionDetailsSubTemplate, appUser,
+     ParticipantsCollection, ParticipantRowView, feedbackOverlayTemplate) {
 
-    var SessionDetailsView = Backbone.View.extend({
+        var SessionDetailsView = Backbone.View.extend({
 
-      events: {
-        "click #subscribe": "handleSessionSubscription",
-        "click #unsubscribe": "handleSessionUnsubscription"
-      },
+            initialize: function(options) {
 
-      initialize: function(options) {
-
-      },
-
-      template: sessionTemplate,
-
-      el: "#section",
+            },
 
 
+            events: {
+                "click #subscribe": "handleSessionSubscription",
+                "click #unsubscribe": "handleSessionUnsubscription",
+                "click #updateFeedback": "handleUpdateFeedback"
+            },
 
-      render: function() {
 
-        var self = this;
+            template: sessionTemplate,
 
-        $(this.el).html(this.template(this.model.toJSON()));
-        var participantsCollection = new ParticipantsCollection(this.model.attributes.participants);
-
-        participantsCollection.each(function(model, index) {          
-          model.attributes.index = index + 1;
-          self.renderParticipantRow(model);
-        });
-      },
-
-      renderParticipantRow: function(model) {
-        var participantRowView = new ParticipantRowView({
-          model: model
-        });
-        $(this.el).find("#sessionParticipants").append(participantRowView.el);
-      },
-
-      handleSessionSubscription: function() {
-
-        console.log(appUser);
-        console.log(this.model.attributes);
+            el: "#section",
 
 
 
+            render: function() {
 
-      },
+                var self = this;
 
-      handleSessionUnsubscription: function() {
-        alert("ji");
-      },
+                $(this.el).html(this.template(this.model.toJSON()));
+                var participantsCollection = new ParticipantsCollection(this.model.attributes.participants);
 
-      addTwo: function(model) {
+                participantsCollection.each(function(model, index) {
+                    model.attributes.index = index + 1;
+                    self.renderParticipantRow(model);
+                });
+            },
 
-        var singlerecord = new participantSub({
-          model: model,
-          collection: this.collection
-        });
+            renderParticipantRow: function(model) {
+                var participantRowView = new ParticipantRowView({
+                    model: model
+                });
+                $(this.el).find("#sessionParticipants").append(participantRowView.el);
+            },
 
-        $(this.el).find("#crud").append(singlerecord.el);
+            handleSessionSubscription: function() {
 
-
-      },
+                console.log(appUser);
+                console.log(this.model.attributes);
 
 
 
 
+            },
 
-      handleErrors: function(error) {
-        $(this.el).find(".error-status").html(error).show();
-      },
+            handleSessionUnsubscription: function() {
+                alert("ji");
+            },
 
-      clearErrors: function() {
-        $(this.el).find(".error-status").empty().hide();
-      },
+            handleUpdateFeedback : function(){
+
+              $("#modalWrapper").html(feedbackOverlayTemplate());
+              $("#feedbackCtrl").modal("show");
+            },
+
+            addTwo: function(model) {
+
+                var singlerecord = new participantSub({
+                    model: model,
+                    collection: this.collection
+                });
+
+                $(this.el).find("#crud").append(singlerecord.el);
 
 
-      addParticipants: function(e) {
-
-        var url = Backbone.history.getFragment().split('/');
-        var timestamp = url[1];
-        console.log(timestamp);
-        var username = AppUser.firstname;
-        var email = AppUser.email;
-
-        var collection = this.collection.participants;
-
-        console.log(collection);
-
-        var participant = new Participant();
-        var that = this;
-
-        participant.set('participant', username);
-        participant.set('email', email);
-        participant.set('ssid', timestamp);
+            },
 
 
 
-        participant.save({
-          wait: true
-        }, {
-          success: function(model, response) {
-
-            console.log(response);
-            that.undelegateEvents();
-
-            // $(self.el).html(sessionconfirmationTemp({topicname : "session successfully added"}));
-            // if(response.errorMessage){
-            //     self.handleErrors(response.errorMessage);
-            //     return;
-            // }
-
-            //  if(response.topicname){     
-
-            //     alert("success");
-
-            //  //    var records=JSON.stringify(user);
-            //  // console.log(records.pid);                   
-            //  //    $(self.el).html(confirmationTemp({topicname : response.topicname}));
-            // }
-            // else{                        
-            // }
-            collection.add(model);
-          },
-          error: function() {
-            console.log("Something went wrong while saving the model");
-            // $(self.el).html(sessionconfirmationTemp({topicname : "something went wrong"}));
-
-          }
 
 
-        });
-      },
+            handleErrors: function(error) {
+                $(this.el).find(".error-status").html(error).show();
+            },
+
+            clearErrors: function() {
+                $(this.el).find(".error-status").empty().hide();
+            },
 
 
-      deleteParticipants: function(e) {
-        var that = this;
-        var collection = this.collection.participants;
-        var ssidd = collection.id;
-        var email = AppUser.email;
-        console.log(collection.id);
-        console.log(collection);
-        console.log(AppUser.email);
+            addParticipants: function(e) {
 
-        collection.each(function(model) {
-          console.log(model.attributes.ssid);
-          console.log(ssidd);
-          if (model.attributes.ssid === parseInt(ssidd)) {
-            if (email == model.attributes.email) {
-              model.destroy({
-                success: function() {
-                  console.log("success");
+                var url = Backbone.history.getFragment().split('/');
+                var timestamp = url[1];
+                console.log(timestamp);
+                var username = AppUser.firstname;
+                var email = AppUser.email;
 
-                  //             // $(e.target).closest("tr").remove();
-                  //             //studentCollection.remove();
-                  that.undelegateEvents();
-                },
-                error: function() {
-                  console.log("here");
-                }
-              });
+                var collection = this.collection.participants;
 
-            } else {
-              console.log("something went wrong");
+                console.log(collection);
+
+                var participant = new Participant();
+                var that = this;
+
+                participant.set('participant', username);
+                participant.set('email', email);
+                participant.set('ssid', timestamp);
+
+
+
+                participant.save({
+                    wait: true
+                }, {
+                    success: function(model, response) {
+
+                        console.log(response);
+                        that.undelegateEvents();
+
+                        // $(self.el).html(sessionconfirmationTemp({topicname : "session successfully added"}));
+                        // if(response.errorMessage){
+                        //     self.handleErrors(response.errorMessage);
+                        //     return;
+                        // }
+
+                        //  if(response.topicname){     
+
+                        //     alert("success");
+
+                        //  //    var records=JSON.stringify(user);
+                        //  // console.log(records.pid);                   
+                        //  //    $(self.el).html(confirmationTemp({topicname : response.topicname}));
+                        // }
+                        // else{                        
+                        // }
+                        collection.add(model);
+                    },
+                    error: function() {
+                        console.log("Something went wrong while saving the model");
+                        // $(self.el).html(sessionconfirmationTemp({topicname : "something went wrong"}));
+
+                    }
+
+
+                });
+            },
+
+
+            deleteParticipants: function(e) {
+                var that = this;
+                var collection = this.collection.participants;
+                var ssidd = collection.id;
+                var email = AppUser.email;
+                console.log(collection.id);
+                console.log(collection);
+                console.log(AppUser.email);
+
+                collection.each(function(model) {
+                    console.log(model.attributes.ssid);
+                    console.log(ssidd);
+                    if (model.attributes.ssid === parseInt(ssidd)) {
+                        if (email == model.attributes.email) {
+                            model.destroy({
+                                success: function() {
+                                    console.log("success");
+
+                                    //             // $(e.target).closest("tr").remove();
+                                    //             //studentCollection.remove();
+                                    that.undelegateEvents();
+                                },
+                                error: function() {
+                                    console.log("here");
+                                }
+                            });
+
+                        } else {
+                            console.log("something went wrong");
+                        }
+
+                    }
+                });
+                // this.model.destroy({
+                //         success:function(){
+                //             console.log("success");
+                //             // $(e.target).closest("tr").remove();
+                //             //studentCollection.remove();
+                //          that.undelegateEvents();
+                //         },
+                //         error:function(){
+                //             console.log("here");
+                //         }
+                //     });
             }
 
-          }
+
+
+
+
         });
-        // this.model.destroy({
-        //         success:function(){
-        //             console.log("success");
-        //             // $(e.target).closest("tr").remove();
-        //             //studentCollection.remove();
-        //          that.undelegateEvents();
-        //         },
-        //         error:function(){
-        //             console.log("here");
-        //         }
-        //     });
-      }
+
+
+        // var participantSub = Backbone.View.extend({
+        //       template: sessionDetailsSubTemplate,
+        //       tagName: 'tr',
+
+
+        //       initialize: function() {
+        //           this.render();
+        //       },
+
+
+        //      render: function() {
+
+        //           var source = this.template(this.model.toJSON());
+        //           console.log(this.model.toJSON());
+        //           $(this.el).html(source);
+
+        //           return this;
+
+        //       }
 
 
 
 
+
+        //   });
+
+
+
+        return SessionDetailsView;
 
     });
-
-
-    // var participantSub = Backbone.View.extend({
-    //       template: sessionDetailsSubTemplate,
-    //       tagName: 'tr',
-
-
-    //       initialize: function() {
-    //           this.render();
-    //       },
-
-
-    //      render: function() {
-
-    //           var source = this.template(this.model.toJSON());
-    //           console.log(this.model.toJSON());
-    //           $(this.el).html(source);
-
-    //           return this;
-
-    //       }
-
-
-
-
-
-    //   });
-
-
-
-    return SessionDetailsView;
-
-  });
